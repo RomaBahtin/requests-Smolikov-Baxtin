1ae91a7d6fd7d54d4660e0613fa16d491756ea99
RomaBahtin <126587462+RomaBahtin@users.noreply.github.com>
2023-03-15 10:40:47 +0300

    task 3

---
+++ Task 3/app3.py
@@ -0,0 +1,19 @@
+import requests
+
+host = 'http://127.0.0.1:5000'
+r = requests.post(host,
+                  json={'a': 1, 'b': 2, 'c': 3})
+print( r.json())
+assert r.json() == {'sum': 6}
+
+r = requests.post(host,
+                  json={'a': 1, 'b': 2})
+
+assert r.json() == {'sum': 3}
+
+r = requests.post(host,
+                  json={'d': 1})
+
+assert r.json() == {'sum': 1}
+
+print(r.text)
